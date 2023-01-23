ba3215007f1b938a55439075b5aced5965f53b9c
leslie ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2023-01-23 12:11:07 +0300

    files

--- 0x02-python-import_modules/3-infinte_add.py
+++ 0x02-python-import_modules/3-infinte_add.py
@@ -1,1 +1,11 @@
-,
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    leng = len(sys.argv)
+    sum = 0
+    for i in range(leng):
+        if i == 0:
+            continue
+        sum += int(sys.argv[i])
+    print('{}'.format(sum))
