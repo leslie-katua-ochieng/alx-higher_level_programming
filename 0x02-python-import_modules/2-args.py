722ec9b8e15a19f249bd72cd6e0f06d39319e854
leslie ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2023-01-23 12:10:04 +0300

    files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,14 @@
-,
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    leng = len(sys.argv)
+    if leng == 1:
+        print('{} arguments.'.format(leng - 1))
+    elif len(sys.argv) == 2:
+        print('{} argument:'.format(leng - 1))
+    else:
+        print('{} arguments:'.format(leng - 1))
+    for i in range(leng):
+        if i == 0:
+            continue
+        print('{:d}: {:s}'.format(i, sys.argv[i]))
