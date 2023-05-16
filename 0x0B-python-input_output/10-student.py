163f6734d08f689dc84e6e06494e5c2c3f3989c1
leslie ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2023-05-16 12:05:52 +0300

    readme

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,28 @@
-,
+#!/usr/bin/python3
+"""
+Class Student
+Public instance attributes:
+- first_name
+- last_name
+- age
+"""
+
+
+class Student():
+    def __init__(self, first_name, last_name, age):
+        """ Constructor """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance
+        check if attrs is a list, if not retrieve None
+        """
+        if isinstance(attrs, list):
+            for item in attrs:
+                if not isinstance(item, str):
+                    return self.__dict__
+            return {key: self.__dict__[key]
+                    for key in attrs if key in self.__dict__}
+        return self.__dict__
