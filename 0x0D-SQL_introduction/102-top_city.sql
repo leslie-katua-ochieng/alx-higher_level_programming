eab574f7594d9b164f1dace74f19c7dc6fec8717
leslie ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2023-05-22 19:11:33 +0300

    readme

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,1 +1,10 @@
-,
+-- during July and August
+-- in desc order
+-- it displays the average temp of 3 cities
+
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
