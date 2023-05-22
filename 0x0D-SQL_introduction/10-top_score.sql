30c944a0e880f523b1e5d5c4a0e3110b557a4fcb
leslie ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2023-05-22 19:08:38 +0300

    readme

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,5 @@
-,
+-- in descending order
+-- lists all rows of the table second_table
+-- from the database hbtn_0c_0
+-- results display score and name
+SELECT score, name FROM second_table ORDER BY score DESC;
