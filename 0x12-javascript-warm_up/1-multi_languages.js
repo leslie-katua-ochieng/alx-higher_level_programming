d05c5a3868ac8ba6a55b833bb28d7904b025fa26
leslie ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2023-05-22 21:35:44 +0300

    readme

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,1 +1,6 @@
-o
+#!/usr/bin/node
+
+// Script that prints C is fun,Python is cool and JavaScript is amazing.
+
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
