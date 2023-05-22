ab62e3380ea9f2f1bbd04a573fd2f67dc4e00b99
leslie ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2023-05-22 19:13:12 +0300

    readme

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,1 +1,7 @@
-,
+-- ordered by score
+-- lists all rows of the table second_table
+-- from the database hbtn_0c_0
+-- results display score and name
+-- with score >= 10
+
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
